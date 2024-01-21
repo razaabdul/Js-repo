7bc94c4eb2a58b4f14ff07f29ac6624e35ab37bf
razaabdul <141930655+razaabdul@users.noreply.github.com>
2024-01-21 15:16:51 +0000

    js-repo

--- base/base.js
+++ base/base.js
@@ -1,1 +1,34 @@
-console.log('hi ');
+/* let
+s ="  my name is abdulraza  "
+console.log(s.charAt(5));
+console.log(s.indexOf('a'));
+console.log(s.toLowerCase(s))
+console.log(s.toUpperCase(s))
+console.log(s.trimStart(s))
+console.log(s.trimEnd(s))
+console.log(s.slice(1,-5));
+console.log(s[5]);
+console.log(s.length);
+*/
+/*
+const score=100.25 
+const balance =  new Number(100)
+console.log(score)
+console.log(balance.toString().length)
+console.log(score.toFixed(2));
+console.log(score.toPrecision(4)) // hive round of decimal
+
+let hudreds = 10000
+console.log(hudreds.toLocaleString()); */
+
+/*
+console.log(Math);
+console.log(Math.abs(-4));// gives always positive value
+console.log(Math.round(6.7));// gives round of values 
+let max=20
+let min=10
+console.log(Math.random()*10+min); // give random between 10 or 20
+*/
+
+
+let mydate= new
