45533cfc3532593c54de35c5591eb1ef1085de24
razaabdul <141930655+razaabdul@users.noreply.github.com>
2024-01-22 11:58:47 +0000

    js-repo

---
+++ base/execution.js
@@ -0,0 +1,34 @@
+let a=10
+let b=5
+
+function raza(num1,num2){
+    let total= num1+num2
+    return total
+}
+let result=raza(a,b)
+let result2=raza(10,2)
+console.log(result);
+console.log(result2);
+
+
+
+function one(){
+    console.log('one');
+    two()                     // it this execution one or two dono execute hokr call stack  me rehenge then first execute function bahar hojyega 
+}
+function two(){
+    console.log('two');
+    three()
+}
+function three(){
+    console.log('three');
+}
+one()
+two()
+three()    
+
+
+// lifo concept runs (last in first out)
+
+// jo bhi phele execute hota hai vo call stack me jata hai execution ke baad call stack se nikal jata hai .
+
